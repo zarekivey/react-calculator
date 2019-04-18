4a495fe38bc3db39aebb5809dd89777ad974fa4a
Malachi Ivey <zarekbusiness@gmail.com>
2019-04-18 17:13:51 -0400

    folders made, button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '.Buttom.css';
+
+const Button = props => (
+    // The text for the button
+    <div>
+        {props.children}
+    </div>
+)
+
+export default Button;
